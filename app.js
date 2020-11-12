f1e7129d498a1fb2940559e8be47e88bae59759e
Paula-333 <71704680+Paula-333@users.noreply.github.com>
2020-11-12 20:54:43 +0100

    heroku pruebas

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const routesMovie = require ('./components/movie/router.js');
 const routesUser = require ('./components/user/router.js');
 const routesOrder = require ('./components/order/router.js');
 
-const PORT = 8204;
+const PORT = 8208;
 
 const mongoose = require('mongoose');
 const MongoURI = process.env.MongoURI||'mongodb://localhost:27017/moviesback';
 mongoose.connect(MongoURI,{useCreateIndex:true,useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=> console.log('mongoose conectado' + MongoURI))
-.catch( e => console.error('mongoose erroneo', e));
+.catch( e => console.error('mongoose erroneo ', e));
 //.then(()=>console.log('conected to Mongodb:'+MongoURI))
 //mongoose.connect(MongoURI)
 
@@ -25,3 +25,7 @@ app.use ('/order/', routesOrder);
 app.use ('/user/', routesUser);
 
 app.listen(PORT,()=>console.log('Servidor levantado en ' + PORT));
+
+['unhandledRejection', 'uncaughtException'].forEach(event => process.on(event, (err) => {
+    console.error(`unhandled error: ${err.stack || err}`);
+})); //yrTKyfTqZYIFi6cH
