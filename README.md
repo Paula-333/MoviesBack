6b925aa4895ea118af5de63cc7c570389a1ef704
Paula <PaulaLoveSmile@MacBook-Pro-de-Paulaa.local>
2020-12-03 13:06:11 +0100

    cors añadido

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Es un proyecto de backEnd de un videoclub donde el usuario puede registrarse y a
 En este proyecto he usado NodeJS, Express y MongoDB.
 
 #### EndPoints:
-- Registro de usuario: localhost:8202/users/
-- Ver perfil:localhost:8202/users/:id
-- Darse de baja:localhost:3200/users/
-- Login:localhost:8202/users/login
-- Crear pedido:localhost:8202/orders/:id
-- Ver todas las peliculas y busqueda por titulo y género: localhost:8202/movie?q=
-- Buscar películas por id: localhost:8202/movies/:id
+- Registro de usuario: https://heroku-moviesbackend.herokuapp.com/user/createUser
+- Ver perfil:localhost:https://heroku-moviesbackend.herokuapp.com//user/findUser/:id
+- Darse de baja:https://heroku-moviesbackend.herokuapp.com/user/remove
+- Login:localhost:https://heroku-moviesbackend.herokuapp.com/user/login
+- Crear pedido:localhost:https://heroku-moviesbackend.herokuapp.com/order/:id
+- Ver todas las peliculas y busqueda por titulo y género:https://heroku-moviesbackend.herokuapp.com/movie?q=
+- Buscar películas por id:https://heroku-moviesbackend.herokuapp.com/movie/:id
 
 
 
--- app.js
+++ app.js
@@ -9,18 +9,22 @@ const routesMovie = require ('./components/movie/router.js');
 const routesUser = require ('./components/user/router.js');
 const routesOrder = require ('./components/order/router.js');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const mongoose = require('mongoose');
 //mongodb+srv://admin1:yrTKyfTqZYIFi6cH@cluster0.dp8tm.mongodb.net/moviesback?retryWrites=true&w=majority
 
-const MongoURI = process.env.MongoURI ||'mongodb://localhost:27017/moviesback';
-const monguri2 = "mongodb+srv://admin1:yrTKyfTqZYIFi6cH@cluster0.dp8tm.mongodb.net/moviesback?retryWrites=true&w=majority"
-mongoose.connect(monguri2,{useCreateIndex:true,useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=> console.log('mongoose conectado' + MongoURI))
+const MONGO_URI = process.env.MONGO_URI ||'mongodb://localhost:27017/moviesback';
+//const monguri2 = "mongodb+srv://admin1:yrTKyfTqZYIFi6cH@cluster0.dp8tm.mongodb.net/moviesback?retryWrites=true&w=majority"
+mongoose.connect(MONGO_URI,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify: false
+})
+.then(()=> console.log('mongoose conectado'))
 .catch( e => console.error('mongoose erroneo ', e));
-//.then(()=>console.log('conected to Mongodb:'+MongoURI))
-//mongoose.connect(MongoURI)
+
 
 app.use(express.json());
 app.use ('/movie/', routesMovie);
@@ -29,6 +33,19 @@ app.use ('/user/', routesUser);
 
 app.listen(PORT,()=>console.log('Servidor levantado en ' + PORT));
 
+
+
+const cors = require('cors');
+app.use(cors());
+app.use(function (req, res, next) { //para evitar el error CORS
+    res.header("Access-Control-Allow-Origin", "*"); //permite hacer peticiones desde todos los orígenes
+    res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"); //permite peticiones con las cabeceras enumeradas
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    //res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
+
 ['unhandledRejection', 'uncaughtException'].forEach(event => process.on(event, (err) => {
     console.error(`unhandled error: ${err.stack || err}`);
-})); //yrTKyfTqZYIFi6cH
+})); 
